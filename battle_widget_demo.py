eca657b8ea55b0620ca8775bc64d9d9d9181f002
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2025-12-16 23:22:19 +0100

    progress

--- battle_widget_demo.py
+++ battle_widget_demo.py
@@ -38,11 +38,11 @@ def _():
         step_dt = traitlets.Float(0.02).tag(sync=True)  # seconds
         move_speed = traitlets.Float(55.0).tag(sync=True)  # pixels / second
 
-        attack_range = traitlets.Float(10.0).tag(sync=True)  # pixels
-        attack_cooldown = traitlets.Float(0.25).tag(sync=True)  # seconds
+        attack_range = traitlets.Float(200.0).tag(sync=True)  # pixels
+        attack_cooldown = traitlets.Float(0.1).tag(sync=True)  # seconds
         hit_chance = traitlets.Float(0.85).tag(sync=True)
         damage = traitlets.Int(1).tag(sync=True)
-        hp = traitlets.Int(1).tag(sync=True)
+        hp = traitlets.Int(3).tag(sync=True)
 
         max_time = traitlets.Float(30.0).tag(sync=True)
         record_dt = traitlets.Float(0.1).tag(sync=True)
@@ -54,13 +54,40 @@ def _():
 
     battle = mo.ui.anywidget(
         BattleWidget(
-            grid_spec={"n_blue": [30, 40, 50], "n_red": [30, 40, 50]},
+            grid_spec={
+                "n_blue": list(range(100, 1000, 50)), 
+                "n_red": list(range(100, 1000, 50))
+            },
             runs_per_point=1,
             record_dt=0.2,
         )
     )
     battle
-    return (battle,)
+    return battle, mo
+
+
+@app.cell
+def _(battle, pl):
+    (
+        pl.DataFrame(battle.results)
+            .group_by("run_id")
+            .agg(
+                pl.col("n_blue").max().alias("blue_max"),
+                pl.col("n_blue").min().alias("blue_min"),
+                pl.col("n_red").max().alias("red_max"),
+                pl.col("n_red").min().alias("red_min"),
+            )
+            .with_columns(
+                blue_diff = pl.col("blue_max") - pl.col("blue_min"),
+                red_diff = pl.col("red_max") - pl.col("red_min"),
+            )
+            .with_columns(
+                start_diff = (pl.col("red_max") - pl.col("blue_max")).abs(),
+                end_diff = (pl.col("blue_diff") - pl.col("red_diff")).abs(),
+            )
+            .plot.scatter("start_diff", "end_diff")
+    )
+    return
 
 
 @app.cell
@@ -69,15 +96,26 @@ def _(battle):
 
     df = pl.DataFrame(battle.results).melt(id_vars=["run_id", "seed", "time"])
     df
-    return (df,)
+    return df, pl
 
 
 @app.cell
-def _(df):
+def _(df, mo):
+    dropdown = mo.ui.dropdown(df["run_id"].unique())
+    dropdown
+    return (dropdown,)
+
+
+@app.cell
+def _(df, dropdown, pl):
     import altair as alt
 
+    run_id = dropdown.value
+
+    _df = df.filter(pl.col("run_id") == run_id) if run_id else df
+
     chart = (
-        alt.Chart(df)
+        alt.Chart(_df)
         .mark_line()
         .encode(x="time:Q", y="value:Q", color="variable:N", detail='run_id')
         .properties(width=640, height=320, title="Army sizes over time")
